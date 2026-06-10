cfd5d9c56915c88a5adfdf0d52e45da3f462518f
Pranjal Kashid <pranjal.kashid@apexon.com>
2026-06-10 13:32:45 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <h1>My Application</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h2>Welcome to React</h2>;
+}
+
+export default Header;
